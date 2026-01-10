e067ff363ef3e465e5e1b274e056bff29ba5c108
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-10 01:39:49 +0100

    Create Author.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/entity/Author.java
@@ -0,0 +1,20 @@
+package com.shotaroi.librarymanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "authors")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+}
+
