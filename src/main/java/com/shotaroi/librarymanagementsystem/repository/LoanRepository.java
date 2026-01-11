946ae64cd3d6fe9b19cf0149819922b1c7c35990
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-11 04:40:18 +0100

    Create LoanRepository.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/repository/LoanRepository.java
@@ -0,0 +1,10 @@
+package com.shotaroi.librarymanagementsystem.repository;
+
+import com.shotaroi.librarymanagementsystem.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    Optional<Loan> findByBookIdAndReturnedAtIsNull(Long bookId);
+}
