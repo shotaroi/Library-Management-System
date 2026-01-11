1a3b801d490be5994cf630f46523797a7d10bcd9
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-12 00:21:48 +0100

    Create BookRepository.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.shotaroi.librarymanagementsystem.repository;
+
+import com.shotaroi.librarymanagementsystem.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+}
