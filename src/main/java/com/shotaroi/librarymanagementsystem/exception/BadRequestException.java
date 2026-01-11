a944282a0b1f509c2d415c88cb3331eb94319753
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-11 04:40:34 +0100

    Create BadRequestException.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/exception/BadRequestException.java
@@ -0,0 +1,5 @@
+package com.shotaroi.librarymanagementsystem.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) { super(message); }
+}
