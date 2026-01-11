046fdae0f0796aecaf3d2455ed3743bc0fa12d96
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-11 04:40:30 +0100

    Create NotFoundException.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.shotaroi.librarymanagementsystem.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
+}
