73a37ca3501def8b04777781899ef99faa6a4cd5
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-13 05:13:26 +0100

    Update Category.java

--- src/main/java/com/shotaroi/librarymanagementsystem/entity/Category.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/entity/Category.java
@@ -2,15 +2,15 @@ package com.shotaroi.librarymanagementsystem.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.aspectj.lang.annotation.control.CodeGenerationHint;
 
 @Entity
 @Table(name = "categories")
 @Getter
 @Setter
-@NoArgsConstructor @AllArgsConstructor
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
