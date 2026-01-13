f027d6285624a25b8d0ff999f76313122cdecf22
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-13 02:15:39 +0100

    Update BookService.java

--- src/main/java/com/shotaroi/librarymanagementsystem/service/BookService.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/service/BookService.java
@@ -1,57 +1,18 @@
 package com.shotaroi.librarymanagementsystem.service;
 
 import com.shotaroi.librarymanagementsystem.dto.BookCreateRequest;
-import com.shotaroi.librarymanagementsystem.dto.BookResponse;
-import com.shotaroi.librarymanagementsystem.entity.Author;
+import com.shotaroi.librarymanagementsystem.dto.BookUpdateRequest;
 import com.shotaroi.librarymanagementsystem.entity.Book;
-import com.shotaroi.librarymanagementsystem.entity.Category;
-import com.shotaroi.librarymanagementsystem.exception.NotFoundException;
-import com.shotaroi.librarymanagementsystem.mapper.BookMapper;
-import com.shotaroi.librarymanagementsystem.repository.AuthorRepository;
-import com.shotaroi.librarymanagementsystem.repository.BookRepository;
-import com.shotaroi.librarymanagementsystem.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.data.domain.*;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class BookService {
-    private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
-    private final CategoryRepository categoryRepository;
+public interface BookService {
+    Book create(BookCreateRequest req);
 
-    @Transactional
-    public BookResponse create(BookCreateRequest req) {
-        Author author = authorRepository.findById(req.authorId())
-                .orElseThrow(() -> new NotFoundException("Author not found:" + req.authorId()));
-        Category category = categoryRepository.findById(req.categoryId())
-                .orElseThrow(() -> new NotFoundException("Author not found: " + req.authorId()));
-        Book book = Book.builder()
-                .title(req.title())
-                .author(author)
-                .category(category)
-                .available(true)
-                .build();
+    Book update(Long id, BookUpdateRequest req);
 
-        return BookMapper.toResponse(bookRepository.save(book));
-    }
+    void delete(Long id);
 
-    @Transactional(readOnly = true)
-    public Page<BookResponse> list(String title, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
-        Page<Book> result = (title == null || title.isBlank())
-                ? bookRepository.findAll(pageable)
-                : bookRepository.findByTitleContainingIgnoreCase(title, pageable);
+    Book getById(Long id);
 
-        return result.map(BookMapper::toResponse);
-    }
-
-    @Transactional(readOnly = true)
-    public BookResponse getById(Long id) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Book not found: " + id));
-        return BookMapper.toResponse(book);
-    }
+    Page<Book> list(Pageable pageable);
 }
