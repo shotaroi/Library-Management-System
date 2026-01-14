f1c81fa931e2e28b29b13e5f5ebc8d510237bd35
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-14 02:34:22 +0100

    Update BookService.java

--- src/main/java/com/shotaroi/librarymanagementsystem/service/BookService.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/service/BookService.java
@@ -1,18 +1,17 @@
 package com.shotaroi.librarymanagementsystem.service;
 
 import com.shotaroi.librarymanagementsystem.dto.BookCreateRequest;
+import com.shotaroi.librarymanagementsystem.dto.BookResponse;
 import com.shotaroi.librarymanagementsystem.dto.BookUpdateRequest;
 import com.shotaroi.librarymanagementsystem.entity.Book;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
-    Book create(BookCreateRequest req);
-
-    Book update(Long id, BookUpdateRequest req);
-
+    BookResponse create(BookCreateRequest req);
+    BookResponse update(Long id, BookUpdateRequest req);
     void delete(Long id);
-
-    Book getById(Long id);
-
-    Page<Book> list(Pageable pageable);
+    BookResponse getById(Long id);
+    Page<BookResponse> list(Pageable pageable);
+    Page<BookResponse> searchByTitle(String query, Pageable pageable);
 }
