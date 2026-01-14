1e831bb8513e271f968e41a5820cf9b2cc88066c
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-14 02:34:24 +0100

    Update CategoryRepository.java

--- src/main/java/com/shotaroi/librarymanagementsystem/repository/CategoryRepository.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/repository/CategoryRepository.java
@@ -3,8 +3,5 @@ package com.shotaroi.librarymanagementsystem.repository;
 import com.shotaroi.librarymanagementsystem.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>  {
-        Optional<Category> findByNameIgnoreCase(String name);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
