c6b6e592e3dead0e21d843b2e5445ad188d79e5e
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-15 22:36:33 +0100

    Update LibraryManagementSystemApplicationTests.java

--- src/test/java/com/shotaroi/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
+++ src/test/java/com/shotaroi/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -1,8 +1,11 @@
 package com.shotaroi.librarymanagementsystem;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LibraryManagementSystemApplicationTests {
 
