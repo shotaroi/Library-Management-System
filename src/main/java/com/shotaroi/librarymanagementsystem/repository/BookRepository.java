d551512b509e565e66e5c17a71efb3af6b118a0b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-17 13:56:38 +0100

    Update BookRepository.java

--- src/main/java/com/shotaroi/librarymanagementsystem/repository/BookRepository.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/repository/BookRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @EntityGraph(attributePaths = {"author", "category"})
+    Page<Book> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"author", "category"})
     Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     boolean existsByIsbn(String isbn);
 }
+
