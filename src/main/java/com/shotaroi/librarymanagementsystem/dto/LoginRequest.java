de78bd32c6d5901eb16d046ad07164fea993936d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-18 03:09:54 +0100

    Create LoginRequest.java

---
+++ src/main/java/com/shotaroi/librarymanagementsystem/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.shotaroi.librarymanagementsystem.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank String username,
+        @NotBlank String password
+) {}
