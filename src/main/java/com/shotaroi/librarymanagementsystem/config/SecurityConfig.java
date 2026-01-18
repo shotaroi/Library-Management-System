8a8aa9ce46f46e0b0975d0768914917306146067
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-18 22:39:48 +0100

    Update SecurityConfig.java

--- src/main/java/com/shotaroi/librarymanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.shotaroi.librarymanagementsystem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -34,4 +36,10 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
