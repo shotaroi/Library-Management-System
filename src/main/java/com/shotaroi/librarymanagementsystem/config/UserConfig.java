5cbd9f9ef34d03f56643e60c11f8a0def9f3c35b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-19 17:36:23 +0100

    Update UserConfig.java

--- src/main/java/com/shotaroi/librarymanagementsystem/config/UserConfig.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/config/UserConfig.java
@@ -1,19 +1,24 @@
 package com.shotaroi.librarymanagementsystem.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@RequiredArgsConstructor
 public class UserConfig {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
                 User.withUsername("admin")
-                        .password("{noop}admin") // dev only
+                        .password(passwordEncoder.encode("admin"))
                         .roles("ADMIN")
                         .build()
         );
