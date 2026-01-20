0118dac43e65c9e953f9f297f1c3920179cce8d1
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-21 00:43:27 +0100

    Update LoanService.java

--- src/main/java/com/shotaroi/librarymanagementsystem/service/LoanService.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/service/LoanService.java
@@ -2,6 +2,7 @@ package com.shotaroi.librarymanagementsystem.service;
 
 import com.shotaroi.librarymanagementsystem.dto.LoanCreateRequest;
 import com.shotaroi.librarymanagementsystem.dto.LoanResponse;
+import com.shotaroi.librarymanagementsystem.entity.Loan;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface LoanService {
     LoanResponse borrow(LoanCreateRequest req);
     LoanResponse returnBook(Long loanId);
     List<LoanResponse> list();
+    List<LoanResponse> listActive();
 }
