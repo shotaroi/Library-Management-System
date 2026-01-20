b9bb25408413e4ad85e6c4c5cbf84b158155f0b3
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-20 11:24:15 +0100

    Update LoanRepository.java

--- src/main/java/com/shotaroi/librarymanagementsystem/repository/LoanRepository.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/repository/LoanRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
-    Optional<Loan> findByBookIdAndReturnedAtIsNull(Long bookId);
+    boolean existsByBookIdAndReturnedAtIsNull(Long bookId);
 }
