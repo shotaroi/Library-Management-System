ec9436adf541a410bf8ac029c28c4fa25be98003
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-21 01:05:31 +0100

    Update LoanService.java

--- src/main/java/com/shotaroi/librarymanagementsystem/service/LoanService.java
+++ src/main/java/com/shotaroi/librarymanagementsystem/service/LoanService.java
@@ -3,12 +3,14 @@ package com.shotaroi.librarymanagementsystem.service;
 import com.shotaroi.librarymanagementsystem.dto.LoanCreateRequest;
 import com.shotaroi.librarymanagementsystem.dto.LoanResponse;
 import com.shotaroi.librarymanagementsystem.entity.Loan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface LoanService {
     LoanResponse borrow(LoanCreateRequest req);
     LoanResponse returnBook(Long loanId);
-    List<LoanResponse> list();
+    Page<LoanResponse> list(Pageable pageable);
     List<LoanResponse> listActive();
 }
